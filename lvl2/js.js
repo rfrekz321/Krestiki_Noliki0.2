785cd3344b23ade98a674e72d9b717ff5848f864
rfrekz321 <34307793+rfrekz321@users.noreply.github.com>
2018-01-10 11:51:54 +0300

    New timer

--- lvl2/js.js
+++ lvl2/js.js
@@ -29,47 +29,38 @@ function reset() {
     location.reload();
 }
 
-var base = 60;
-var clocktimer, dateObj, dh, dm, ds, ms;
-var readout = '';
-var h = 1,
-    m = 1,
-    tm = 1,
-    s = 0,
-    ts = 0,
-    ms = 0,
-    show = true,
-    init = 0,
-    ii = 0;
+function secudomer() {
+    var chasov = 0;
+    var minut = 0;
+    var sekund = 0;
+    var result = "";
+    var stopwatch_value = 0;
+    var interval;
+    this.start = function(start_from) {
+        stopwatch_value = start_from || 0;
+        interval = setInterval(function() {
+            stopwatch_value++;
 
-function startTIME() {
-    var cdateObj = new Date();
-    var t = (cdateObj.getTime() - dateObj.getTime()) - (s * 1000);
-    if (t > 999) { s++; }
-    if (s >= (m * base)) {
-        ts = 0;
-        m++;
-    } else {
-        ts = parseInt((ms / 100) + s);
-        if (ts >= base) { ts = ts - ((m - 1) * base); }
-    }
-    if (m > (h * base)) {
-        tm = 1;
-        h++;
-    } else {
-        tm = parseInt((ms / 100) + m);
-        if (tm >= base) { tm = tm - ((h - 1) * base); }
-    }
-    ms = Math.round(t / 10);
-    if (ms > 99) { ms = 0; }
-    if (ms == 0) { ms = '00'; }
-    if (ms > 0 && ms <= 9) { ms = '0' + ms; }
-    if (ts > 0) { ds = ts; if (ts < 10) { ds = '0' + ts; } } else { ds = '00'; }
-    dm = tm - 1;
-    if (dm > 0) { if (dm < 10) { dm = '0' + dm; } } else { dm = '00'; }
-    dh = h - 1;
-    if (dh > 0) { if (dh < 10) { dh = '0' + dh; } } else { dh = '00'; }
-    readout = dh + ':' + dm + ':' + ds + '.' + ms;
-    if (show == true) { document.TestForm.stopwatch.value = readout; }
-    clocktimer = setTimeout("startTIME()", 1);
+
+            chasov = Math.floor(stopwatch_value / 3600);
+            minut = Math.floor((stopwatch_value - chasov * 3600) / 60);
+            sekund = stopwatch_value - (chasov * 3600 + minut * 60);
+
+            result = "<b>" + chasov + " часов " + minut + " мин " + sekund + " cек</b>";
+
+            var SummDok1 = document.getElementById('secudomer1');
+            SummDok1.innerHTML = result;
+
+        }, 1000);
+    };
+
+    this.stop = function() {
+        clearInterval(interval);
+    };
+
+
+
 }
+
+var Secudomer = new secudomer();
+Secudomer.start(0);
